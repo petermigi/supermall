d0e2781a6a172c598eea5d4d8f4a7cb31c19a640
peterliu_glb <peterliu_glb@163.com>
2019-10-20 13:42:35 +0800

    11-(掌握)项目开发-tabbar引入和项目模块划分

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
-  <div id="app">    
+  <div id="app">
+    <router-view/>
+    <main-tab-bar/>   
   </div>
 </template>
 
 <script>
+  //import MainTabBar from 'components/content/mainTabbar/MainTabBar'
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
 
   export default {
     name: 'app',
     components: {      
-      
+      MainTabBar
     }
   }
 </script>
 
 <style>
-  @import "./assets/css/base.css"
+  @import "assets/css/base.css"
 </style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h2>首页</h2>
+</template>
+
+<script>  
+
+  export default {
+    name: "Home",
+    components: {
+      
+    },
+    data() {
+      
+    }
+    
+  }
+</script>
+
+<style scoped>
+ 
+</style>
